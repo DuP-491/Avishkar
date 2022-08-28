f6c71e9b0d302d2121f9303167034916edae7e7e
Priyav Kaneria <priyavkaneria@gmail.com>
2022-08-28 20:28:27 +0530

    Setup base game in react successfully

---
+++ client/src/game/consts.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+export enum EVENTS_NAME {
+  gameEnd = 'game-end',
+  chestLoot = 'chest-loot',
+  attack = 'attack'
+}
+
+export enum GameStatus {
+  WIN,
+  LOSE
+}
--- client/src/game/index.tsx
+++ client/src/game/index.tsx
@@ -1,42 +1,20 @@
 import React, { useRef, useState } from 'react';
-import Phaser from 'phaser';
+// import Phaser from 'phaser';
 import { IonPhaser } from '@ion-phaser/react';
+import { gameConfig } from './config';
 
 interface Props {
   viewport: string;
 }
 
-const game = {
-  width: '100%',
-  height: '100%',
-  type: Phaser.AUTO,
-  scene: {
-    init: function () {
-      this.cameras.main.setBackgroundColor('#24252A');
-    },
-    create: function () {
-      this.helloWorld = this.add.text(
-        this.cameras.main.centerX,
-        this.cameras.main.centerY,
-        'Hello World',
-        {
-          font: '40px Arial',
-          fill: '#ffffff'
-        }
-      );
-      this.helloWorld.setOrigin(0.5);
-    },
-    update: function () {
-      this.helloWorld.angle += 1;
-    }
-  } as any
-};
+const game = gameConfig;
 
 function GameComponent(props: Props) {
   const { viewport } = props;
   const gameRef = useRef(null);
   // Call `setInitialize` when you want to initialize your game! :)
   const [initialize, setInitialize] = useState(false);
+  console.log(game);
   const destroy = () => {
     if (gameRef.current) {
       (gameRef.current as any).destroy();
