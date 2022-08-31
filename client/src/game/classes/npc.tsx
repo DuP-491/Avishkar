8980bbf66ed86c9cfc9b3008e1ed150bb336ec92
Priyav Kaneria <priyavkaneria@gmail.com>
2022-09-01 00:07:41 +0530

    NPC interaction with player completed

--- client/src/game/classes/npc.tsx
+++ client/src/game/classes/npc.tsx
@@ -7,6 +7,7 @@ import { Player } from './player';
 export class NPC extends Actor {
   private target: Player;
   private INTERACT_RADIUS = 100;
+  private interacting = false;
   private attackHandler: () => void;
 
   constructor(
@@ -50,14 +51,22 @@ export class NPC extends Actor {
   }
 
   preUpdate(): void {
-    // if (
-    //   Math.Distance.BetweenPoints(
-    //     { x: this.x, y: this.y },
-    //     { x: this.target.x, y: this.target.y }
-    //   ) < this.INTERACT_RADIUS
-    // ) {
-    //   this.askInteract();
-    // }
+    if (
+      Math.Distance.BetweenPoints(
+        { x: this.x, y: this.y },
+        { x: this.target.x, y: this.target.y }
+      ) < this.INTERACT_RADIUS
+    ) {
+      if (!this.interacting) {
+        this.interacting = true;
+        this.askInteract(this.name);
+      }
+    } else {
+      if (this.interacting) {
+        this.interacting = false;
+        this.resetInteract(this.name);
+      }
+    }
   }
 
   public setTarget(target: Player): void {
