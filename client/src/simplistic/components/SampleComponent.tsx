cc153c8acecefaa9f926b2aea9add06415ddac7f
Sanskar Omar <heysanskar0.0@gmail.com>
2022-10-11 22:10:29 +0530

    initialized Branch

---
+++ client/src/simplistic/components/SampleComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SampleComponent = () => {
+  return <div>SampleComponent</div>;
+};
+
+SampleComponent.propTypes = {};
+
+export default SampleComponent;
