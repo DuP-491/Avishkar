5aa85eec189cce29360be31a1d6f595844416e3d
Priyav Kaneria <priyavkaneria@gmail.com>
2022-10-12 14:37:59 +0530

    NPC Chat icon and data

---
+++ client/src/game/npcData.ts
@@ -0,0 +1,16 @@
+export const npcData = {
+  'cafe-1': {
+    name: 'Parth',
+    location: 'cafe-top-right',
+    type: 'text',
+    text: "Hey, I'm Parth. I'm a software engineer and I love to code."
+  },
+  'cafe-2': {
+    name: 'Parth',
+    location: 'cafe-top-left',
+    type: 'ask',
+    text: 'Are you sure you want to register?',
+    success: 'Function will be imported in this file and called',
+    failure: 'Function will be imported in this file and called'
+  }
+};
