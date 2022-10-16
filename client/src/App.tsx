5c8f088cf14d411d78f35ebcb21e6b3e90c57b54
Priyav Kaneria <priyavkaneria@gmail.com>
2022-10-16 17:23:47 +0530

    Fixed build issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 /* eslint-disable */
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Simplistic from './simplistic';
 import GameLayout from './GameLayout';
 /* eslint-enable */
