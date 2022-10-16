11f7d3c4f797d381ae7e64ad644468428e146d32
Priyav Kaneria <priyavkaneria@gmail.com>
2022-10-16 12:44:44 +0530

    Made computer dynamic part 1

--- client/src/simplistic/components/Authentication/SignupBox.tsx
+++ client/src/simplistic/components/Authentication/SignupBox.tsx
@@ -31,12 +31,132 @@ function SignupBox({ onCrossPress, onSignup, onToggle }: SignupBoxPropType) {
   const NameRef = useRef(document.createElement('input'));
   const EmailRef = useRef(document.createElement('input'));
   const CollegeNameRef = useRef(document.createElement('input'));
-  const GenderRef = useRef(document.createElement('input'));
+  const GenderRef = useRef(document.createElement('select'));
   const MobileRef = useRef(document.createElement('input'));
 
   return (
     <>
-      <div className="w-2/3 max-w-xl bg-gray-900/[0.5] h-3/5 rounded-lg flex flex-col p-10">
+      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-gray-900 bg-opacity-50 backdrop-blur-sm py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <img
+              // eslint-disable-next-line no-undef
+              src={require('../../../images/cross-icon.png')}
+              className="absolute cursor-pointer right-4 top-4 invert"
+              onClick={onCrossPress}
+            />
+            <div className="flex space-x-4 justify-center items-center sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                className="h-12 w-auto"
+                src="https://i.imgur.com/cHH4xIh.png"
+                alt="Avishkar Logo"
+              />
+              <h2 className="text-center text-2xl font-bold tracking-tight text-white">Sign Up</h2>
+            </div>
+            <form className="space-y-6 mt-6" onSubmit={LoggingIn} method="POST">
+              <div>
+                <label htmlFor="name" className="block text-sm font-medium text-gray-50">
+                  Name
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="name"
+                    ref={NameRef}
+                    required
+                    className="block w-full min-w-[22rem] bg-gray-200 bg-opacity-50 appearance-none rounded-md border border-gray-200 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-slate-100 focus:outline-none focus:ring-slate-100 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-50">
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    ref={EmailRef}
+                    required
+                    className="block w-full min-w-[22rem] bg-gray-200 bg-opacity-50 appearance-none rounded-md border border-gray-200 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-slate-100 focus:outline-none focus:ring-slate-100 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="college" className="block text-sm font-medium text-gray-50">
+                  College Name
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="college"
+                    name="college"
+                    type="text"
+                    autoComplete="college"
+                    ref={CollegeNameRef}
+                    required
+                    className="block w-full min-w-[22rem] bg-gray-200 bg-opacity-50 appearance-none rounded-md border border-gray-200 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-slate-100 focus:outline-none focus:ring-slate-100 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="gender" className="block text-sm font-medium text-gray-50">
+                  Gender
+                </label>
+                <div className="mt-1">
+                  <select
+                    id="gender"
+                    name="gender"
+                    autoComplete="gender"
+                    ref={GenderRef}
+                    required
+                    className="block w-full min-w-[22rem] bg-gray-200 bg-opacity-50 appearance-none rounded-md border border-gray-200 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-slate-100 focus:outline-none focus:ring-slate-100 sm:text-sm">
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>None</option>
+                  </select>
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="mobile" className="block text-sm font-medium text-gray-50">
+                  Mobile
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="mobile"
+                    name="mobile"
+                    type="mobile"
+                    autoComplete="mobile"
+                    ref={MobileRef}
+                    required
+                    className="block w-full min-w-[22rem] bg-gray-200 bg-opacity-50 appearance-none rounded-md border border-gray-200 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-slate-100 focus:outline-none focus:ring-slate-100 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <button
+                  type="submit"
+                  className="flex mt-2 w-fit justify-center rounded-md border border-transparent bg-slate-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                  Sign up
+                </button>
+                <button
+                  onClick={onToggle}
+                  className="flex mt-2 w-fit justify-center rounded-md border border-transparent bg-slate-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                  Already Registered?
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* <div className="w-2/3 max-w-xl bg-gray-900/[0.5] h-3/5 rounded-lg flex flex-col p-10">
         <button className="text-white self-end" onClick={onCrossPress}>
           X
         </button>
@@ -113,7 +233,6 @@ function SignupBox({ onCrossPress, onSignup, onToggle }: SignupBoxPropType) {
               </label>
             </div>
             <div className="md:w-2/3">
-              {/* TODO: Replace input with dropdown containing values ["male", "female", "none"] */}
               <input
                 className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                 id="inline-gender"
@@ -159,7 +278,7 @@ function SignupBox({ onCrossPress, onSignup, onToggle }: SignupBoxPropType) {
             </div>
           </div>
         </form>
-      </div>
+      </div> */}
     </>
   );
 }
