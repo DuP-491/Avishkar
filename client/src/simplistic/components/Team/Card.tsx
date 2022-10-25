5279d9af04a7fb2d0bacd1cf3bdf1a52cc3d2a4d
Parthtrap <mttparth306@gmail.com>
2022-10-25 17:28:16 +0530

    minor Teams Page Animation

--- client/src/simplistic/components/Team/Card.tsx
+++ client/src/simplistic/components/Team/Card.tsx
@@ -8,8 +8,12 @@ interface CardProps {
 
 const Card = ({ imgsrc, name, designation }: CardProps) => {
   return (
-    <div className="flex flex-col items-center p-4 scale-75 sm:p-6 rounded-xl hover:scale-100 duration-75 hover:shadow-2xl">
-      <img className="object-cover w-full aspect-square rounded-full" src={imgsrc} alt="" />
+    <div className="flex flex-col items-center p-4 duration-100 scale-75 w-80 sm:p-6 rounded-xl hover:scale-100 hover:shadow-2xl">
+      <img
+        className="object-cover w-full duration-200 rounded-full hover:rounded-xl hover:scale-110 aspect-square"
+        src={imgsrc}
+        alt=""
+      />
 
       <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">
         {name}
