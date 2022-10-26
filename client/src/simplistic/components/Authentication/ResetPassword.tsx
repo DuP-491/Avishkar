090ff0f961395671a09d70ee9c2623563dccf6ac
Parthtrap <mttparth306@gmail.com>
2022-10-26 07:01:34 +0530

    Confirm Password Field Added

--- client/src/simplistic/components/Authentication/ResetPassword.tsx
+++ client/src/simplistic/components/Authentication/ResetPassword.tsx
@@ -9,6 +9,11 @@ const ResetPassword = () => {
   function PasswordReset(e: any) {
     e.preventDefault();
     const password = PasswordRef.current.value;
+    const confirmpassword = ConfirmPasswordRef.current.value;
+    if (confirmpassword != password) {
+      console.log('Confirm password does not match the password');
+      return;
+    }
     let token;
     if (key === undefined) {
       console.log('key===undefined');
@@ -28,6 +33,7 @@ const ResetPassword = () => {
       });
   }
   const PasswordRef = useRef(document.createElement('input'));
+  const ConfirmPasswordRef = useRef(document.createElement('input'));
 
   return (
     <div
@@ -65,6 +71,22 @@ const ResetPassword = () => {
                   id="password"
                   placeholder="••••••••"
                   ref={PasswordRef}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required={true}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  ref={ConfirmPasswordRef}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required={true}
                 />
