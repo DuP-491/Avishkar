00aa9643474ae5a08209b3dcc1ceeba60e61c3fd
Sanskar Omar <heysanskar0.0@gmail.com>
2022-10-31 09:47:54 +0530

    Removed Faq from Nav

--- client/src/simplistic/components/Common/ResponsiveNav.tsx
+++ client/src/simplistic/components/Common/ResponsiveNav.tsx
@@ -69,14 +69,14 @@ const ResponsiveNav = () => {
                 Team
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
                 to="/faq"
                 onClick={() => setIsMobileNavOpen(false)}
                 className="block py-2 pl-3 pr-4 text-gray-400 rounded md:border-0 md:p-0 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
                 FAQ
               </Link>
-            </li>
+            </li> */}
             {Cookies.get('token') ? (
               <>
                 <li>
