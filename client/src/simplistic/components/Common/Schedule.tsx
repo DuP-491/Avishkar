e491687849509ea5dd17f4230a85e574373fca4f
lovedeep singh kamal <developer.lovedeep@gmail.com>
2022-11-09 16:50:45 +0530

    added schedule images

--- client/src/simplistic/components/Common/Schedule.tsx
+++ client/src/simplistic/components/Common/Schedule.tsx
@@ -1,38 +1,72 @@
-import React, { useEffect } from 'react';
-import ViewSDKClient from './AdobePdfSdkClient';
-import Spinner from './Spinner';
+import React, { useState } from 'react';
 
 const Schedule = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://documentcloud.adobe.com/view-sdk/main.js';
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-  useEffect(() => {
-    const viewSDKClient = new ViewSDKClient();
-    viewSDKClient.ready().then(() => {
-      // not my id , found it on documentation
-      viewSDKClient.previewFile('pdf-div', {}, process.env.REACT_APP_ADOBE_PDF_KEY);
-    });
-  }, []);
-
+  const [activeTab, setActiveTab] = useState(0);
   return (
-    <div className="flex items-center justify-center w-full h-screen text-center text-white uppercase bg-gray-900 ">
-      {/* <iframe
-        // src="https://docs.google.com/spreadsheets/d/1YHbhIHTUSo2c0l6M-nOqT4lEtRGGeWIqyywoExSSXmQ/edit?embedded=true"
-        src="https://docs.google.com/spreadsheets/d/1zFNlU-2xKXr6jNe50GZsqBfkwKW4XA8Y70DTArAwDzc/edit?embedded=true"
-        className="w-full m-4"
-        width="640"
-        height="718"></iframe> */}
-
-      <div id="pdf-div" className="full-window-div">
-        <Spinner displayTxt="loading.." />
-      </div>
+    <div className="w-full min-h-screen p-4 text-center text-white uppercase bg-gray-900">
+      <ul className="max-w-xl mx-auto text-sm font-medium text-center text-gray-400 divide-gray-700 rounded-lg shadow sm:flex">
+        <li className="w-full">
+          <a
+            onClick={() => setActiveTab(0)}
+            className={`inline-block w-full p-4 ${
+              activeTab === 0 ? 'text-white bg-gray-700' : 'bg-gray-800'
+            } sm:rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none hover:text-white hover:bg-gray-700`}
+            aria-current="page">
+            10 NOV
+          </a>
+        </li>
+        <li className="w-full">
+          <a
+            onClick={() => setActiveTab(1)}
+            className={`inline-block w-full p-4 ${
+              activeTab === 1 ? 'text-white bg-gray-700' : 'bg-gray-800'
+            } focus:ring-4 focus:ring-blue-300 focus:outline-none hover:text-white hover:bg-gray-700`}>
+            11 NOV
+          </a>
+        </li>
+        <li className="w-full">
+          <a
+            onClick={() => setActiveTab(2)}
+            className={`inline-block w-full p-4 ${
+              activeTab === 2 ? 'text-white bg-gray-700' : 'bg-gray-800'
+            } focus:ring-4 focus:ring-blue-300 focus:outline-none hover:text-white hover:bg-gray-700`}>
+            12 NOV
+          </a>
+        </li>
+        <li className="w-full">
+          <a
+            onClick={() => setActiveTab(3)}
+            className={`inline-block w-full p-4 ${
+              activeTab === 3 ? 'text-white bg-gray-700' : 'bg-gray-800'
+            } sm:rounded-r-lg focus:ring-4 focus:outline-none focus:ring-blue-300 hover:text-white hover:bg-gray-700`}>
+            13 NOV
+          </a>
+        </li>
+      </ul>
+      {activeTab === 0 && (
+        <div className="max-w-xl mx-auto mt-4 space-y-4">
+          <img src={require('../../Assets/s1.png')} alt="day1_1_schedule" />
+          <img src={require('../../Assets/s2.png')} alt="day1_2_schedule" />
+        </div>
+      )}
+      {activeTab === 1 && (
+        <div className="max-w-xl mx-auto mt-4 space-y-4">
+          <img src={require('../../Assets/s3.png')} alt="day1_1_schedule" />
+          <img src={require('../../Assets/s4.png')} alt="day1_2_schedule" />
+        </div>
+      )}
+      {activeTab === 2 && (
+        <div className="max-w-xl mx-auto mt-4 space-y-4">
+          <img src={require('../../Assets/s5.png')} alt="day1_1_schedule" />
+          <img src={require('../../Assets/s6.png')} alt="day1_2_schedule" />
+          <img src={require('../../Assets/s7.png')} alt="day1_1_schedule" />
+        </div>
+      )}
+      {activeTab === 3 && (
+        <div className="max-w-xl mx-auto mt-4 space-y-4">
+          <img src={require('../../Assets/s8.png')} alt="day1_2_schedule" />
+        </div>
+      )}
     </div>
   );
 };
